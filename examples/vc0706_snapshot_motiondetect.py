829b179fb06ef17a2b007bf70426c733ae006b4b
Alec Delaney <89490472+tekktrik@users.noreply.github.com>
2022-06-17 09:20:27 -0400

    Add motion detect example

---
+++ examples/vc0706_snapshot_motiondetect.py
@@ -0,0 +1,42 @@
+import board
+import busio
+import adafruit_vc0706
+
+# Create a serial connection for the VC0706 connection, speed is auto-detected.
+uart = busio.UART(board.TX, board.RX)
+# Setup VC0706 camera
+vc0706 = adafruit_vc0706.VC0706(uart)
+
+# Print the version string from the camera.
+print("VC0706 version:")
+print(vc0706.version)
+
+# Set the baud rate to 115200 for fastest transfer (its the max speed)
+vc0706.baudrate = 115200
+
+# Set the image size.
+vc0706.image_size = adafruit_vc0706.IMAGE_SIZE_160x120  # Or set IMAGE_SIZE_320x240 or
+# IMAGE_SIZE_160x120
+# Note you can also read the property and compare against those values to
+# see the current size:
+size = vc0706.image_size
+if size == adafruit_vc0706.IMAGE_SIZE_640x480:
+    print("Using 640x480 size image.")
+elif size == adafruit_vc0706.IMAGE_SIZE_320x240:
+    print("Using 320x240 size image.")
+elif size == adafruit_vc0706.IMAGE_SIZE_160x120:
+    print("Using 160x120 size image.")
+
+vc0706.set_motion_detect(1)
+print("detecting motion:")
+print(vc0706.get_motion_detect())
+
+while True:
+    if vc0706.motion_detected():
+        print("Motion!")
+        print("disable motion detect")
+        vc0706.set_motion_detect(0)
+        print("enable motion detect")
+        vc0706.set_motion_detect(1)
+    else:
+        print("....")
